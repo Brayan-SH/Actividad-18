913c3e284edf2bde8fc34857cb1659660c29e2b1
Brayan-SH <116335684+Brayan-SH@users.noreply.github.com>
2025-09-06 04:33:58 -0600

    Actual commit message

--- Consurso_bandas.py
+++ Consurso_bandas.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 
 # Inscribir bandas por categoría.
@@ -5,36 +6,165 @@ import tkinter as tk
 # Listar bandas inscritas.
 # Generar un ranking final.
 
+# Nombre de la banda, institución(Primaria, Básico, Diversificado).
 class Banda_Participante:
-  pass
+  @property
+  def nombre(self):
+    return self.__nombre
+
+  @property
+  def institucion(self):
+    return self.__institucion
+  def __init__(self, nombre, institucion):
+    self.__nombre = nombre
+    self.__institucion = institucion
+
+  def mostrar_info(self):
+    return (
+      f'>Banda Participante : '
+      f'Nombre: {self.nombre}'
+      f'Institución: {self.institucion}'
+    )
 
 
-class Banda_Escolar:
+class Banda_Escolar(Banda_Participante):
   def __init__(self, nombre, institucion, categoria):
-    self.nombre = nombre
-    self.institucion = institucion
-    self.categoria = categoria
+    super().__init__(nombre, institucion)
+    self.__categoria = None # Primaria, Básico, Diversificado
+    self.__puntajes = {}
+    self.set_categoria(categoria)
+
+  
+  # Validar categoría : Primaria, Básico, Diversificado
+  def Set_Categoria(self, categoria):
+    categoria_aux = True if categoria in ['Primaria', 'Basico', 'Diversificado'] else False
+    if categoria_aux :
+      self.__categoria = categoria
+      return self.__categoria
+    else :
+      print('Categoría inválida. Debe ser Primaria, Básico o Diversificado.')
+      return None
+  
+  
+  # Criterios de evaluación (0 a 10): 
+  # • ritmo,
+  # • uniformidad, 
+  # • coreografía, 
+  # • alineación, 
+  # • puntualidad.
+  # Puntaje total = suma de criterios.
+  def Registrar_Puntaje(self, puntaje):
+    if 0 <= puntaje <= 10:
+      self.__puntajes.append(puntaje)
+    else:
+      return None
+      # return None
+  
+  
+  # Propiedades total y promedio.
+  def Total_Puntaje(self):
+    return sum(self.__puntajes)
+
+
+  # Promedio de puntajes.
+  def Promedio_Puntaje(self):
+    if len(self.__puntajes) == 0:
+      return 0
+    return sum(self.__puntajes) / len(self.__puntajes)
+
+
+  def Mostrar_Info(self):
+    info = super().Mostrar_Info()
+    info += f'Categoría: {self.__categoria}\n'
+    info += f'Total Puntaje: {self.Total_Puntaje()}\n' if self.__puntajes else 'No evaluada aún.\n'
+    return info
+
+
+class Consurso_Bandas(Banda_Escolar):
+  def __init__(self):
+    self.bandas = {}
+    
+  def Inscribir_Banda(self, banda: Banda_Escolar):
+    if banda.nombre not in self.bandas:
+      self.bandas[banda.nombre] = banda
+      print(f'Banda {banda.nombre} inscrita exitosamente.')
+  
+  
+   # Criterios de evaluación (0 a 10): 
+  # • ritmo,
+  # • uniformidad, 
+  # • coreografía, 
+  # • alineación, 
+  # • puntualidad.
+  # Puntaje total = suma de criterios.
+  def Registrar_Evaluacion(self, nombre_banda, puntajes):
+    while True:
+      Banda_Escolar.Limpiar()
+      print('Criterios de evaluación (0 a 10) : ')
+      ritmo = int(input('• Ritmo : '))
+      if 0 <= ritmo <= 10:
+        Banda_Escolar.Registrar_Puntaje(ritmo)
+        break
+
+    while True:
+      Banda_Escolar.Limpiar()
+      print('Criterios de evaluación (0 a 10) : ')
+      uniformidad = int(input('• Uniformidad : '))
+      if 0 <= uniformidad <= 10:
+        Banda_Escolar.Registrar_Puntaje(uniformidad)
+        break
+    while True:
+      Banda_Escolar.Limpiar()
+      print('Criterios de evaluación (0 a 10) : ')
+      coreografia = int(input('• Coreografía : '))
+      if 0 <= coreografia <= 10:
+        Banda_Escolar.Registrar_Puntaje(coreografia)
+        break
+    while True:
+      Banda_Escolar.Limpiar()
+      print('Criterios de evaluación (0 a 10) : ')
+      alineacion = int(input('• Alineación : '))
+      if 0 <= alineacion <= 10:
+        Banda_Escolar.Registrar_Puntaje(alineacion)
+        break
+    while True:
+      Banda_Escolar.Limpiar()
+      print('Criterios de evaluación (0 a 10) : ')
+      puntualidad = int(input('• Puntualidad : '))
+      if 0 <= puntualidad <= 10:
+        Banda_Escolar.Registrar_Puntaje(puntualidad)
+        break
+
 
   # Listar bandas inscritas.
   def Listar_Bandas(self):
-    return f"{self.nombre} - {self.institucion} - {self.categoria}"
-  
-  
-# Inscribir banda : nombre de la banda, institución y categoría(Primaria, Basico, Diversificado).
-class Inscribir_por_Categoria(Banda_Escolar) :
-  pass
+    with open('bandas_inscritas.txt', 'r') as file:
+      for line in file:
+        campos = line.strip().split(',')
+        print(f'Nombre : {campos[0]}')
+        print(f'Institución : {campos[1]}')
+        print(f'Categoría : {campos[2]}')
 
 
-# Criterios de evaluación (0 a 10): ritmo, uniformidad, coreografía, alineación, puntualidad.
-# Puntaje total = suma de criterios.
-class Puntaje_por_Criterio(Banda_Escolar) :
-  pass
+  def Generar_Ranking_Final():
+    pass
 
+# Inscribir banda por categoria : nombre de la banda, institución.
+  def Inscribir_por_Categoria(self, categoria) :
+    self.__categoria = Banda_Escolar.set_categoria(categoria)
+    bandera = False
+    with open('bandas_inscritas.txt', 'r') as file:
+      for line in file:
+        campos = line.strip().split(',')
+        if self.nombre == campos[0] :
+          bandera = True
+          print('La banda ya está inscrita.')
+          break
+        
+      if not bandera :
+        with open('bandas_inscritas.txt', 'a') as file:
+          file.write(f'{self.nombre},{self.institucion},{self.__categoria}\n')
+          print('Banda inscrita exitosamente.')
 
-# Retorna las bandas ordenadas por total y criterios de desempate.
-class Generar_Ranking_Final(Banda_Escolar) :
-  pass
-
-
-class Consurso_Bandas:
-  pass
+  def Limpiar() :
+    os.system('cls' if os.name == 'nt' else 'clear')
--- README.md
+++ README.md
@@ -35,3 +35,60 @@ Parámetros de estilo en Tkinter
 
 6. Estado
   > • state: estado del widget. Opciones: "normal", "active", "disabled".
+
+
+Actividad 18
+La Municipalidad de Quetzaltenango organizará el concurso de bandas del desfile del 14 de septiembre. Se necesita un programa en Python, usando POO, que permita gestionar la inscripción y evaluación de bandas escolares.
+
+■ OBJETIVO
+• Implementar clases, herencia, encapsulación y polimorfismo para:
+
+• Inscribir bandas por categoría.
+• Registrar puntajes por criterios.
+• Listar bandas inscritas.
+• Generar un ranking final. retorna las bandas ordenadas por total y criterios de desempate.
+
+■ REQUISITOS FUNCIONALES
+• Registrar bandas con: nombre de la banda, institución y categoría.
+• Categorías válidas: Primaria, Básico, Diversificado.
+• Criterios de evaluación (0 a 10): ritmo, uniformidad, coreografía, alineación, puntualidad.
+• Puntaje total = suma de criterios.
+
+■ REQUISITOS TECNICOS DE POO
+1. Clase base Participante con:
+    • nombre 
+    • institucion
+    • método mostrar_info().
+
+2. Clase BandaEscolar(Participante) con:
+    • Atributos protegidos: _categoria y _puntajes.
+    • Método set_categoria(categoria) que valide la categoría.
+    • Método registrar_puntajes(diccionario) que valide criterios y rangos (0–10).
+    • Propiedades total 
+    • promedio.
+    • Sobrescribe mostrar_info() para incluir la categoría y el total si ya fue evaluada.
+
+3. Clase Concurso con:
+    • inscribir_banda(banda) (no permitir dos bandas con el mismo nombre).
+    • registrar_evaluacion(nombre_banda, puntajes).
+    • listar_bandas() imprime todas las bandas y sus puntajes si ya fueron evaluadas.
+    • ranking() retorna las bandas ordenadas por total y criterios de desempate.
+
+■ FLUJO MINIMO EXIGIDO
+• Crear tres bandas (una por cada categoría).
+• Inscribirlas en el concurso.
+• Registrar puntajes válidos para cada una.
+• Mostrar listado general.
+• Mostrar ranking final.
+
+■ REGLAS DE VALIDACION
+• Rechazar categoría inválida.
+• Rechazar puntajes fuera de 0–10 o con criterios incompletos.
+• No permitir bandas con el mismo nombre.
+
+■ EJEMPLO DE INTERACCION (REFERENCIAL)
+• Crear banda “Liceo Xela” (Básico), “Estrella Infantil” (Primaria), “Águilas del Occidente” (Diversificado).
+• Inscribir en Concurso("Concurso de Bandas - 15 de Septiembre", "2025-09-15").
+• Registrar puntajes para cada banda.
+• Imprimir listar_bandas() y luego el ranking() mostrando: posición, nombre, institución, categoría y total.
+
